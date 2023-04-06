480ed12ddd4afbcb42ab0a4618b3d03f16e79ec5
Giuseppe Garzone <98554253+CuocoBasilio@users.noreply.github.com>
2023-04-06 15:59:31 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+ "use strict"
+ 
+//google map
+/*
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;*/
+
+ScrollReveal({ reset: true });
+//ScrollReveal().reveal('#banner', { delay: 500, origin: top });
+ScrollReveal().reveal('.title-head', { delay: 600, origin: left });
